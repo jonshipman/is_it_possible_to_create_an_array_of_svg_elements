fa74c992aea3cadc08931c4f33be5ca8b79295e0
Jon Shipman <jon@jonshipman.com>
2024-01-31 10:19:05 -0600

    init

---
+++ src/lib/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as TwitterIcon } from './twitter.svelte';
+export { default as FacebookIcon } from './facebook.svelte';
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { FacebookIcon, TwitterIcon } from '$lib/icons';
+	import type { ComponentType } from 'svelte';
+
+	type Socials = Array<[string, ComponentType]>;
+	const socials: Socials = [
+		['https://www.twitter.com', TwitterIcon],
+		['https://www.facebooks.com', FacebookIcon]
+	];
+</script>
+
+<h1 class="mb3">Icons</h1>
+
+<div class="flex">
+	<a href="https://www.twitter.com" class="pink">
+		<TwitterIcon class="w3 h3" />
+	</a>
+
+	<a href="https://www.facebook.com" class="pink">
+		<FacebookIcon class="w3 h3" />
+	</a>
+</div>
+
+<h2 class="mb3">...or in a loop.</h2>
+
+<div class="flex">
+	{#each socials as [link, component]}
+		<a href={link} class="pink">
+			<svelte:component this={component} class="w3 h3" />
+		</a>
+	{/each}
+</div>
